b120b9a7ddf26511f72341926e38873078a610a6
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-10-28 14:37:34 +0200

    ajout du logo

--- emailmanagement/src/App.vue
+++ emailmanagement/src/App.vue
@@ -5,15 +5,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome to Emailo" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/coucou">coucou</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
@@ -30,6 +30,7 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+  width: auto;
 }
 
 nav {
