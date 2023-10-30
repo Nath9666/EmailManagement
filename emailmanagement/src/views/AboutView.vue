f89fb654939273fbcc3c60385367e8c26d916b2b
laura saada <laura.saada@efrei.net>
2023-10-30 13:12:42 +0100

    Amélioration de la home Page et about page + add un backgroud color + suppression de deux fichier plus utilisé

--- emailmanagement/src/views/AboutView.vue
+++ emailmanagement/src/views/AboutView.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="title">Here comes the content of our the AboutPage</h1>
+    <p>So the group is composed of:</p>
+    <ul>
+      <li v-for="name in GroupPeopleName" :key="name">{{ name }}</li>
+    </ul>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script>
+export default {
+  name: "AboutPage",
+  beforeUnmount() {
+    // Réafficher le header lorsque le composant est démonté
+    const header = document.querySelector("header");
+    if (header) {
+      header.style.display = "block";
+    }
+  },
+  components: {},
+  props: { user: Object },
+  data() {
+    return {
+      GroupPeopleName: ["Nathan MOREL", "Laura SADAA", "Miggao GONG"],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: black;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+.title {
+  text-align: center;
+  font-size: 29px;
+  margin-bottom: 25px;
 }
 </style>
